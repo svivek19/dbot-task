f86ba28408c46b3172fa103d5903178b486fadfb
svivek19 <svivek030503@gmail.com>
2025-07-03 18:17:59 +0530

    fix: update API base URL to production endpoint

--- client/src/services/employeeService.js
+++ client/src/services/employeeService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://dbot-task.onrender.com/api",
 });
 
 export const getEmployees = () => api.get("/employees");
